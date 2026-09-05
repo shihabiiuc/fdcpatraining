cdc3035b20b74a666e56be527364064c6cb1e103
suh <shihabdinajpur@gmail.com>
2026-09-05 23:43:23 +0600

    Added: sitemap (auto) and GSC

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,15 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://fdcpatraining.com',
-	vite: {
-		plugins: [tailwindcss()],
+  site: 'https://fdcpatraining.com',
+
+  vite: {
+      plugins: [tailwindcss()],
 	},
+
+  integrations: [sitemap()],
 });
